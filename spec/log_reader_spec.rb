4c365e2504e17415706dd4a3aaccae92971caae2
Fiodar Turliuk <fiodar_turliuk@epam.com>
2019-03-14 16:40:00 +0300

    add spec for puts page visits

--- spec/log_reader_spec.rb
+++ spec/log_reader_spec.rb
@@ -1,14 +1,15 @@
 require 'spec_helper'
+require 'active_support/core_ext/kernel/reporting'
 require_relative '../lib/log_reader'
 
 describe LogReader do
 
-  let(:file) { 'home/user/logfile.log' }
+  let(:command_line_args) { "--file ruby.rb #{file}" }
+  before { ARGV = command_line_args.split }
 
   describe '#initialize' do
     context 'reads ARGV from command line' do
-      let(:command_line_args) { '--file ruby.rb home/user/logfile.log' }
-      before { ARGV = command_line_args.split }
+      let(:file) { Dir.pwd + '/spec/fixtures/logfile.log' }
 
       it 'chooses file with right extension from ARGV' do
         expect(described_class.new.file).to eq file
@@ -18,9 +19,17 @@ describe LogReader do
 
   describe '.most_page_views' do
     context 'calls PageViewsCounter' do
+      let(:file) { Dir.pwd + '/spec/fixtures/logfile.log' }
+      let(:output_file) { Dir.pwd + '/spec/fixtures/output.txt' }
+      let(:output) { File.read(file)  }
+
       it do
         expect(PageViewsCounter).to receive(:call).with(file)
         described_class.most_page_views
+      end
+
+      it 'prints pages with views counts in descending order' do
+        expect(described_class.most_page_views).to output.to_stdout
       end
     end
   end
