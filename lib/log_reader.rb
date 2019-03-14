9a77606701704278803573b6b359f11bf3504a19
Fiodar Turliuk <fiodar_turliuk@epam.com>
2019-03-14 15:07:35 +0300

    refactor log_reader

--- lib/log_reader.rb
+++ lib/log_reader.rb
@@ -1,6 +1,10 @@
 class LogReader
   def initialize
-    @file = ARGV.select { |arg| /([a-zA-Z0-9\s_\\.\-\(\):])+(.log)$/ =~ arg }.first
+    @file = file_name_from_argv
   end
   attr_reader :file
+
+  def file_name_from_argv
+    ARGV.select { |arg| /([a-zA-Z0-9\s_\\.\-\(\):])+(.log)$/ =~ arg }.first
+  end
 end
