1a1ea246f86ef5d01e4b42c18fb4ed39a210a280
Fiodar Turliuk <fiodar_turliuk@epam.com>
2019-03-14 16:01:33 +0300

    fix page_views_counter_spec

--- lib/page_views_counter.rb
+++ lib/page_views_counter.rb
@@ -1,5 +1,16 @@
 class PageViewsCounter
   def self.call(file)
+    new(file).call
+  end
 
+  def initialize(file)
+    @file = file
+  end
+  attr_reader :file
+
+  def call
+    a = Hash.new(0)
+    IO.readlines(file).each { |line| a[line.split.first.to_sym] += 1  }
+    a
   end
 end
