9fd087443c8d06757b8e453aef92c934d4e2f0ea
Fiodar Turliuk <fiodar_turliuk@epam.com>
2019-03-15 12:02:31 +0300

    fix unique_page_views_counter

--- lib/unique_page_views_counter.rb
+++ lib/unique_page_views_counter.rb
@@ -2,11 +2,16 @@ require_relative 'page_views_counter'
 class UniquePageViewsCounter < PageViewsCounter
   private
 
-  # returning page name with IP here just to be more verbose about why pages
-  # with the same name are shown multiple times in unique views
   def build_page_views_count_hash
-    IO.readlines(file).reduce(Hash.new(0)) do |acc, line|
-      acc[line] += 1
+    build_map_url_to_ips.reduce({}) do |acc, item|
+      acc[item.first] = item[1].uniq.count
+      acc
+    end
+  end
+
+  def build_map_url_to_ips
+    IO.readlines(file).reduce(Hash.new { |h, k| h[k] = [] }) do |acc, line|
+      acc[line.split.first] << line.split[1]
       acc
     end
   end
