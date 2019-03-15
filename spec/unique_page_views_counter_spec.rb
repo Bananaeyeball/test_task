7c70128bbcc242ac70198d2177c829e3034d167d
Fiodar Turliuk <fiodar_turliuk@epam.com>
2019-03-15 12:02:07 +0300

    changed spec logic

--- spec/unique_page_views_counter_spec.rb
+++ spec/unique_page_views_counter_spec.rb
@@ -8,16 +8,9 @@ describe UniquePageViewsCounter do
 
     let(:hash) do
       {
-        "/home 316.433.849.805\n" => 2,
-        "/help_page/1 126.318.035.038\n" => 1,
-        "/home 184.123.665.067\n" => 1,
-        "/about 444.701.448.104\n" => 1,
-        "/help_page/1 929.398.951.889\n" => 1,
-        "/help_page/1 722.247.931.582\n" => 1,
-        "/about 061.945.150.735\n" => 1,
-        "/help_page/1 646.865.545.408\n" => 1,
-        "/home 235.313.352.950\n" => 1,
-        "/help_page/1 543.910.244.929\n" => 1
+        "/home" => 4,
+        "/help_page/1" => 5,
+        "/about" => 2,
       }
     end
 
