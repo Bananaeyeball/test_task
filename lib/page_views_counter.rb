eba8aee01dfffbf47456dd783122daad77aa44ce
Fiodar Turliuk <fiodar_turliuk@epam.com>
2019-03-15 09:22:20 +0300

    refactored page_views_counter

--- lib/page_views_counter.rb
+++ lib/page_views_counter.rb
@@ -15,10 +15,6 @@ class PageViewsCounter
   private
 
   def build_page_views_count_hash
-    a = Hash.new(0)
-    IO.readlines(file).each { |line| a[line.split.first.to_sym] += 1  }
-    a
-    # dont get why this doesn't work
-    # IO.readlines(file).reduce(Hash.new(0)) { |acc, line| acc[line.split.first.to_sym] += 1  }
+    IO.readlines(file).reduce(Hash.new(0)) { |acc, line| acc[line.split.first.to_sym] += 1; acc  }
   end
 end
