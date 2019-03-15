540d14db70782ce4d0e5e5b177f55fb6925256f7
Fiodar Turliuk <fiodar_turliuk@epam.com>
2019-03-15 13:18:57 +0300

    add page_views_printer

---
+++ lib/page_views_printer.rb
@@ -0,0 +1,33 @@
+class PageViewsPrinter
+  def self.call(page_views_hash, unique_page_views_hash)
+    new(page_views_hash, unique_page_views_hash).call
+  end
+
+  def initialize(page_views_hash, unique_page_views_hash)
+    @page_views_hash = page_views_hash
+    @unique_page_views_hash = unique_page_views_hash
+  end
+  attr_reader :page_views_hash, :unique_page_views_hash
+
+  def call
+    print_page_views
+    puts "="*100 + "\n"
+    print_unique_page_views
+  end
+
+  private
+
+  def print_page_views
+    puts "All Page Views\n"
+    page_views_hash.group_by do |k, v|
+      v
+    end.each { |k, v| v.each { |v| puts "#{v.join(" ")} visits" } }
+  end
+
+  def print_unique_page_views
+    puts "Unique Page Views\n"
+    unique_page_views_hash.each do |k, v|
+      puts "#{k} #{v} visits"
+    end
+  end
+end
